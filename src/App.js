be84e68e67ec05887410989e0da7bd71a695fcd6
Ray Maldonado <rm01964@rmn.com>
2020-07-16 13:38:22 -0500

    fix(component): this will replace the text that was mispelled in the main screen
    This will apply a fix for the mispelled text that was put in the main screen by an intern that came
    and destory our company

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and make sure to save it to reload few times.
+          Edit <code>src/App.js</code> and make sure to save it to reload few times or none.
         </p>
         <a
           className="App-link"
