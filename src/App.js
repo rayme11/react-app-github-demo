2ac33f66c188c6eae7983bf0ff39b525af3fa9c6
Ray Maldonado <rm01964@rmn.com>
2020-07-16 13:31:18 -0500

    fix: change text in main page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and make sure to save it to reload.
+          Edit <code>src/App.js</code> and make sure to save it to reload few times.
         </p>
         <a
           className="App-link"
